f27e155f3addbf4112338956b8d10b3e18599895
Android Studio <Android@uirapuru.br>
2026-08-18 11:07:09 -0300

    Continuação

---
+++ app/src/main/java/com/thayna/via_cep/CEP.java
@@ -0,0 +1,15 @@
+package com.thayna.via_cep;
+
+public class CEP {
+    private String cep;
+
+    private String logradouro;
+
+    private String complemento;
+
+    private String cidade;
+
+    private String localidade;
+
+    private String uf;
+}
